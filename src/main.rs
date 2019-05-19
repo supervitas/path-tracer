75a05776922ccfa8a1e4280f62b750d0044f00e4
Nikolaev <nikolaev.vitally@yandex.ru>
2019-05-19 03:47:48 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod math;
+
+fn main() {
+    let camera_position = math::vec3::Vector3{x:0.0, y:0.0, z: 5.0};
+}
---
+++ src/math/vec3.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
