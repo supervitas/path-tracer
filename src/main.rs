4d1a35b7cdc7e59db7efb7eaa704facbc37ead3b
Nikolaev <nikolaev.vitally@yandex.ru>
2019-05-23 13:18:52 +0300

    generic vec3 wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod math;
 
 fn main() {
-    let camera_position = math::vec3::Vector3{x:0.0, y:0.0, z: 5.0};
+    let camera_position = math::vec3::Vector3{x:2, y:2, z: 5};
+    let camera_position2 = math::vec3::Vector3{x:2, y:1, z: 15};
+
+    let t = camera_position.dot(camera_position2);
+
+    println!("{}", t);
 }
--- src/math/vec3.rs
+++ src/math/vec3.rs
@@ -1,7 +1,39 @@
-#[derive(Debug)]
-pub struct Vector3 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64
+use std::fmt;
+use std::ops::{Mul, Add};
+
+pub struct Vector3 <T: Copy> {
+    pub x: T,
+    pub y: T,
+    pub z: T
 }
 
+//impl<S> Vector3<S> where S: Mul<Output=S> + Add<Output=S> + Copy {
+//    pub fn dot(&self, v2: Vector3<S>) -> S {
+//        self.x * v2.x + self.y * v2.y + self.z * v2.z
+//    }
+//}
+
+impl<S> Vector3<S> where S: Into<f64> + Copy {
+    pub fn dot<U>(&self, v2: Vector3<U>) -> f64 where U: Into<f64> + Copy {
+        self.x.into() * v2.x.into() + self.y.into() * v2.y.into() + self.z.into() * v2.z.into()
+    }
+}
+
+impl<T: Add<Output = T>> Add for Vector3<T>  where T: Copy + Mul, <T as Mul>::Output: Add<Output=T> {
+    type Output = Vector3<T>;
+
+    fn add(self, other: Vector3<T>) -> Vector3<T> {
+        Vector3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        }
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for Vector3<T>
+    where T: Copy + Mul, <T as Mul>::Output: Add<Output=T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}, z: {}", self.x, self.y, self.z)
+    }
+}
